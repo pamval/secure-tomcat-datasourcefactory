5a8db4c586fbc5aabf4826c5b3bc1eb45bd794ea
Pedro Vale <pedro.vale@hitachivantara.com>
2018-03-07 17:23:15 +0000

    Fixed concurrency issue where more than one datasource using this factory would not work since they would all use the first one password

--- src/main/java/com/github/ncredinburgh/tomcat/SecureDataSourceFactory.java
+++ src/main/java/com/github/ncredinburgh/tomcat/SecureDataSourceFactory.java
@@ -1,11 +1,12 @@
 package com.github.ncredinburgh.tomcat;
 
-import static com.github.ncredinburgh.tomcat.PasswordHolder.getPassword;
-import static com.github.ncredinburgh.tomcat.PasswordHolder.setPassword;
 import static com.github.ncredinburgh.tomcat.PropertyParser.parseProperties;
 import static javax.xml.bind.DatatypeConverter.parseBase64Binary;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.naming.Context;
 import javax.sql.DataSource;
@@ -20,6 +21,7 @@ import org.apache.tomcat.jdbc.pool.DataSourceFactory;
 public class SecureDataSourceFactory extends DataSourceFactory {
 		
 	private Decryptor decryptor = new ConfigurableDecryptor();
+	private Map<String, String> passwordCache = new ConcurrentHashMap<String, String>( );
 			
 	@Override
 	public DataSource createDataSource(Properties properties, Context context, boolean XA) throws Exception {
@@ -29,11 +31,21 @@ public class SecureDataSourceFactory extends DataSourceFactory {
 		return super.createDataSource(properties, context, XA);
 	}
 
+	private String getPassword ( String key ) {
+		return passwordCache.getOrDefault( key, null );
+	}
+
+	private void setPassword (String key, String password) {
+		passwordCache.put( key, password );
+
+	}
+
 	private void replacePassword(Properties properties) throws DecryptionException {
-		if (getPassword() == null) {
-			setPassword(decryptPassword(properties));
+		String dataSourceKey = properties.getProperty(PROP_URL) + ";" + properties.getProperty(PROP_PASSWORD) + ";" + properties.getProperty(PROP_CONNECTIONPROPERTIES);
+		if (getPassword( dataSourceKey ) == null) {
+			setPassword( dataSourceKey, decryptPassword( properties ) );
 		}
-		properties.setProperty(PROP_PASSWORD, getPassword());
+		properties.setProperty(PROP_PASSWORD, getPassword( dataSourceKey ));
 	}
 	
 	private String decryptPassword(Properties properties) throws DecryptionException {
